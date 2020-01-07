f7b2abe2343e93ed82e7d1e3b9203ca1b20a26dd
ironest <ironest@gmail.com>
2020-01-07 16:52:56 +1100

    Added event handlers

--- src/App.js
+++ src/App.js
@@ -1,26 +1,81 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import './App.css'; 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    username: "",
+    password: "",
+    extraUsernameInfo: "",
+    extraPasswordInfo: ""
+  }
+
+  inputUserOnChange = (event) => {
+
+    const {value} = event.target;
+
+    this.setState({
+      username: value,
+      extraUsernameInfo: ""
+    });
+    if (value.length > 0 && value.length < 5){
+      this.setState({extraUsernameInfo: "At least 5 chars long"});
+    }
+  }
+
+  inputPassOnChange = (event) => {
+
+    const {value} = event.target;
+
+    this.setState({
+      password: value,
+      extraPasswordInfo: ""
+    });
+
+    if ( value.length > 0
+         &&
+         (value.toLowerCase() === value
+         ||
+         value.toUpperCase() === value)
+        ){
+      this.setState({extraPasswordInfo: "Use both upper and lower case"});
+    }
+  
+  }
+
+  render() {
+
+    const { username, password, extraUsernameInfo, extraPasswordInfo } = this.state
+
+    return (
+      <div className="container">
+        <div className="panel">
+
+          <p className="alertMsg">{extraUsernameInfo}</p>
+          <label>Username</label>
+          <input type="text" onChange={this.inputUserOnChange} />
+
+          <p className="alertMsg">{extraPasswordInfo}</p>
+          <label>Password</label>
+          <input type="password" onChange={this.inputPassOnChange} />
+        </div>
+
+        <div className="panel">
+
+          <div className="idCard">
+            <h2>ID Card</h2>
+            { username && <img className="profilePic" src={`https://robohash.org/${username}.png?set=set1&size=250x250`} /> }
+            <p>Username: {username}</p>
+            <p>Password: {password}</p>
+          </div>
+        </div>
+        <div></div>
+      </div>
+    );
+
+  }
+
+
 }
 
 export default App;
